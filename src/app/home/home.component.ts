08ff9ee5a8c666a00873b89905edaf3e03cd3d27
Felix Fermin <felix.gomez@rappi.com>
2021-04-17 17:58:32 -0500

    home page with banner feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  mySwiper: Swiper | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.mySwiper = new Swiper('.swiper-container');
+  }
 }
